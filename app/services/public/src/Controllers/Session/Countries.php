17c1caf223fc47e3c2dd5cd829e267e1dee93d46
Furqan Siddiqui <hello@furqansiddiqui.com>
2022-11-14 00:10:57 +0400

    get countries list endpoint

---
+++ app/services/public/src/Controllers/Session/Countries.php
@@ -0,0 +1,30 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Services\Public\Controllers\Session;
+
+use App\Common\Countries\CachedCountriesList;
+
+/**
+ * Class Countries
+ * @package App\Services\Public\Controllers\Session
+ */
+class Countries extends AbstractSessionAPIController
+{
+    protected function sessionAPICallback(): void
+    {
+    }
+
+    /**
+     * @return void
+     * @throws \Comely\Database\Exception\ORM_Exception
+     * @throws \Comely\Database\Exception\SchemaTableException
+     */
+    public function get(): void
+    {
+        $countriesList = CachedCountriesList::getInstance(useCache: true, availableOnly: true);
+
+        $this->status(true);
+        $this->response->set("countries", $countriesList);
+    }
+}
